772f0ab5401a2e644d250a2470b57c7a1d20fce4
ttechgirl <abiodunayisat2@gmail.com>
2023-03-01 23:39:02 +0100

    changes

--- WeatherForecastApp/Services/WeatherService.cs
+++ WeatherForecastApp/Services/WeatherService.cs
@@ -13,13 +13,23 @@ namespace WeatherForecastApp.Services
         }
         public async Task<OpenWeatherResponseModel> GetCity(string name)
         {
+            //returns an empty response when nothing is passed to the parameter {name} 
+            if (string.IsNullOrEmpty(name))
+            {
+                return null;
+            }
+
             var key = configuration.GetValue<string>("OpenWeather:SecretKey");
+
             var uri = $"{configuration.GetValue<string>("OpenWeather:url")}/weather?q={name}&appid={key}";
 
 
             using (var client = new HttpClient())
             {
                 var request = await client.GetAsync(uri);
+
+                /*returns an empty response when the cityname passed to the parameter {name}
+                is wrong or not found*/
 
                 if (!request.IsSuccessStatusCode)
                 {
@@ -30,6 +40,7 @@ namespace WeatherForecastApp.Services
                 var result = JsonConvert.DeserializeObject<OpenWeatherResponseModel>(response);
                 result.weather.FirstOrDefault();
                 return result;
+          
             }
         }
     }
